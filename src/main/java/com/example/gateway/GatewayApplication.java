3fef781b4370ccf7367e5503ab00f17b9725b370
Artorias718 <lai.ndr94@gmail.com>
2022-04-09 19:59:07 +0200

    corretto e generalizzato path di routing per app-service

--- src/main/java/com/example/gateway/GatewayApplication.java
+++ src/main/java/com/example/gateway/GatewayApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
 public class GatewayApplication {
